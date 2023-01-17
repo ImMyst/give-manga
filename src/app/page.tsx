26143b1bb76b3c5ce723ee83b8fa0d59cbd932cb
ImMyst <titouan.galvani@yeap.ai>
2023-01-17 22:11:17 +0100

    feat: install tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Inter } from '@next/font/google'
-
 export default function Home() {
   return (
-    <main>
+    <main className="text-red-400 bg-gray-800 flex w-screen h-screen items-center justify-center">
       Hello from Next 13
     </main>
-  )
+  );
 }
