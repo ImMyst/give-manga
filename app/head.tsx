5b956491301421221627e7a5b22573b08c0a314a
ImMyst <titouan.galvani@yeap.ai>
2023-01-31 21:28:03 +0100

    docs: add keywords and update description

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,15 @@ export default function Head() {
     <>
       <title>Give Manga</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Get a random manga in one button" />
+      <meta
+        name="description"
+        content="Get a random manga or anime in one button"
+      />
+      <meta
+        name="keywords"
+        content="Manga, Anime, API, Random, Next, Tailwind, React"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  );
 }
