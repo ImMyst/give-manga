cba9deb94a7bd1ccf4c210c39b6361ce09081d2e
ImMyst <titouan.galvani@yeap.ai>
2023-01-31 21:18:36 +0100

    fix: fixing colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,16 +7,22 @@ import { usePathname } from "next/navigation";
 export default function Navbar() {
   const pathname = usePathname();
   const IS_ANIME = pathname?.includes("anime");
+
   return (
     <nav>
       <Link
         className={cn(
-          pathname ? "decoration-orange-600" : "decoration-blue-600",
+          IS_ANIME ? "decoration-blue-600" : "decoration-orange-600",
           "text-gray-200 text-xl hover:underline hover:text-yellow-50"
         )}
         href={IS_ANIME ? "/" : "/anime"}
       >
-        Switch to {IS_ANIME ? "Manga" : "Anime"}
+        Switch to{" "}
+        {IS_ANIME ? (
+          <span className="text-blue-300">Manga</span>
+        ) : (
+          <span className="text-orange-300">Anime</span>
+        )}
       </Link>
     </nav>
   );
